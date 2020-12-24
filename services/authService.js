00a07722db16db550594044521811031153616e8
Nguyen Viet Thai <vietthai2512@gmail.com>
2020-12-24 08:43:32 +0700

    fix: refactor signUp

--- services/authService.js
+++ services/authService.js
@@ -5,37 +5,54 @@ const { JWT_ACCESS } = require('../config')
 
 exports.signUp = async function (newUser)
 {
-    let [hashedPassword, existsEmail] = await Promise.all(
-        [
-            argon2.hash(newUser.password, { type: argon2.argon2id }),
-            db.users.existsEmail(newUser.email)
-        ]
-    );
-
-    if (existsEmail)
+    try
     {
-        throw new Error('This email address is not available. Choose a different address.');
+        let [hashedPassword, existsEmail] = await Promise.all(
+            [
+                argon2.hash(newUser.password, { type: argon2.argon2id }),
+                db.users.existsEmail(newUser.email)
+            ]
+        );
+
+        if (existsEmail)
+        {
+            throw new Error('This email address is not available. Choose a different address.');
+        }
+
+        newUser.password = hashedPassword;
+
+        const userRecord = await db.users.insert(newUser);
+
+        if (!userRecord)
+        {
+            throw new Error('User cannot be created');
+        }
+
+        const createdUser = userRecord;
+        Reflect.deleteProperty(createdUser, 'password');
+
+        return createdUser;
     }
-
-    newUser.password = hashedPassword;
-
-    return await db.users.insert(newUser);
+    catch (e)
+    {
+        throw e;
+    }
 }
 
 exports.logIn = async function (user)
 {
-    const result = await db.users.findByEmail(user.email);
+    const userRecord = await db.users.findByEmail(user.email);
 
-    if (result === null)
+    if (userRecord === null)
     {
         throw new Error('Your email or password was incorrect.');
     }
 
-    if (await argon2.verify(result.password, user.password))
+    if (await argon2.verify(userRecord.password, user.password))
     {
         console.log('Correct password');
         const payloadToken = {
-            id: result.id,
+            id: userRecord.id,
             email: user.email
         }
         return generateAccessToken(payloadToken);
