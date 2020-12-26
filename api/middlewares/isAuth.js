5f015345fd723307d54a67581f9e59cd857ce1b6
Nguyen Viet Thai <vietthai2512@gmail.com>
2020-12-27 03:09:01 +0700

    fix: remove get token from header

--- api/middlewares/isAuth.js
+++ api/middlewares/isAuth.js
@@ -1,16 +1,9 @@
 const jwt = require('express-jwt');
 const { JWT_ACCESS, JWT_REFRESH } = require('../../config');
 
-const getTokenFromHeader = (req) => 
+const getTokenFromCookies = (req) => 
 {
-    if (
-        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') ||
-        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token')
-    )
-    {
-        return req.headers.authorization.split(' ')[1];
-    }
-    return null;
+    //if(req.cookies)
 }
 
 module.exports = jwt(
@@ -18,7 +11,7 @@ module.exports = jwt(
         secret: JWT_ACCESS.SECRET,
         algorithms: [JWT_ACCESS.ALGORITHM],
         requestProperty: 'token',
-        getToken: getTokenFromHeader
+        getToken: getTokenFromCookies
     }
 );
 
