25121c80e8bae662cb50047330b30d0a2759116a
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-03 01:28:34 +0700

    add return type to repos/users.ts

--- src/database/repos/users.ts
+++ src/database/repos/users.ts
@@ -25,48 +25,56 @@ export class UsersRepo
         this.createColumnSets(pgp);
     }
 
-    async insert(newUser: object)
+    // Insert a new user, and return the new object
+    async insert(newUser: object): Promise<User>
     {
         return this.db.one(this.pgp.helpers.insert(newUser, cs.insert) + 'RETURNING id');
     }
 
-    async empty()
+    // Remove all records from the table
+    async empty(): Promise<null>
     {
         return this.db.none(usersSQL.empty);
     }
 
-    async delete(userID: string)
+    // Delete an user by ID, return that ID
+    async delete(userID: string): Promise<string>
     {
         return this.db.one(usersSQL.delete, userID);
     }
 
-    async all()
+    // Get all user records
+    async all(): Promise<User[]>
     {
         return this.db.any(usersSQL.all, { table: 'users' });
     }
 
-    async count()
+    // Count the total number of users
+    async count(): Promise<number>
     {
         return this.db.one(usersSQL.count, { table: 'users' }, a => +a.count);
     }
 
-    async update(userUpdate: object)
+    // Update user's infomation 
+    async update(userUpdate: object): Promise<User>
     {
         const query = this.pgp.helpers.update(userUpdate, cs.insert) + this.pgp.as.format(' WHERE id = ${id} RETURNING *', userUpdate);
         return this.db.one(query);
     }
 
-    async existsEmail(email: string)
+    // Check if an email is already registered
+    async existsEmail(email: string): Promise<boolean>
     {
         return this.db.one(usersSQL.exists, { column: 'email', table: 'users', columnData: email }).then(result => result.exists);
     }
 
-    async findByEmail(email: string)
+    // Find an user by email
+    async findByEmail(email: string): Promise<User | null>
     {
         return this.db.oneOrNone(usersSQL.find, { table: 'users', column: 'email', columnData: email });
     }
 
-    async findByID(id: string)
+    async findByID(id: string): Promise<User | null>
     {
         return this.db.oneOrNone(usersSQL.find, { table: 'users', column: 'id', columnData: id });
     }
