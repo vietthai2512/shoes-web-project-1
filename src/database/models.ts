e2b703e92e3cc69ed59f3d78b54c5f97a631f35d
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-03 22:28:23 +0700

    fix optional property User interface

--- src/database/models.ts
+++ src/database/models.ts
@@ -1,12 +1,12 @@
 export interface User
 {
-    id: string,
-    email: string,
-    password: string,
+    id?: string,
+    email?: string,
+    password?: string,
     first_name?: string,
     last_name?: string,
     middle_name?: string,
     phone_number?: string,
     join_date?: Date,
-    user_role: string;
+    user_role?: string;
 }
