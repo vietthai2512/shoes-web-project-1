0ddd16edf191ccb6b5ca2adf99b8de0d1d743c00
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-07 23:50:40 +0700

    copy some sql files

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,3 +1,6 @@
 import shell from 'shelljs';
 
 shell.cp('-R', 'src/public', 'dist/public');
+shell.cp('-R', 'src/database/sql/common', 'dist/database/sql/common');
+shell.cp('-R', 'src/database/sql/products', 'dist/database/sql/products');
+shell.cp('-R', 'src/database/sql/users', 'dist/database/sql/users');
