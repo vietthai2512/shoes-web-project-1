e69adfed6b466a0ecdcabd2405906d345f5e47ce
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-10 22:13:52 +0700

    remove all js legacy files

--- src/api/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-module.exports =
-{
-    isAuth: require('./isAuth'),
-    attachCurrentUser: require('./attachCurrentUser')
-}
--- src/database/repos/index.js
+++
@@ -1,5 +0,0 @@
-module.exports =
-{
-    Users: require('./users')
-
-}
