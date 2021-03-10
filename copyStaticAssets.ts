84f7b2b4654a855da4f58e6a034bf93cdc00354b
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-10 22:33:07 +0700

    force to remove and re-copy static files to dist

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,13 @@
 import shell from 'shelljs';
 
+shell.rm('-rf', 'dist/public');
 shell.cp('-R', 'src/public', 'dist/public');
+
+shell.rm('-rf', 'dist/database/sql/common');
 shell.cp('-R', 'src/database/sql/common', 'dist/database/sql/common');
+
+shell.rm('-rf', 'dist/database/sql/products');
 shell.cp('-R', 'src/database/sql/products', 'dist/database/sql/products');
+
+shell.rm('-rf', 'dist/database/sql/users');
 shell.cp('-R', 'src/database/sql/users', 'dist/database/sql/users');
