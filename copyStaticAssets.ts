e16db560466c962126eef8ca4841ed48bb1a5b49
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-03-25 21:49:43 +0700

    fix copy static assets

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,7 +1,10 @@
 import shell from 'shelljs';
 
-shell.rm('-rf', 'dist/public');
-shell.cp('-R', 'src/public', 'dist/public');
+shell.rm('-rf', 'dist/public/images');
+shell.cp('-R', 'src/public/images', 'dist/public/images');
+
+shell.rm('-rf', 'dist/public/stylesheets');
+shell.cp('-R', 'src/public/stylesheets', 'dist/public/stylesheets');
 
 shell.rm('-rf', 'dist/database/sql/common');
 shell.cp('-R', 'src/database/sql/common', 'dist/database/sql/common');
